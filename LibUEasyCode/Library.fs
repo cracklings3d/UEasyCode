69cb76e5184dbbb993cbf3791c05ad36040e6a9a
Cracklings home <Cracklings3D@gmail.com>
2022-11-21 02:10:43 +0800

    First

---
+++ LibUEasyCode/Library.fs
@@ -0,0 +1,41 @@
+﻿namespace LibUEasyCode
+
+open System
+open Newtonsoft.Json
+open Newtonsoft.Json.Linq
+
+
+module Say =
+  let hello name = printfn $"Hello %s{name}"
+
+module UProject =
+  type EngineVersion =
+    | LauncherVersion of version : Version
+    | SourceBuild of uuid : Guid
+
+  type Platform =
+    | Win64 = 0x01
+    | Win32 = 0x02
+    | Android = 0x04
+    | Linux = 0x08
+    | MacOS = 0x10
+
+  type UProjectDescriptor =
+    {
+      supported_platforms : Platform
+      engine_version : EngineVersion
+    }
+    
+  let analyse descriptor =
+    let c = JObject.Parse descriptor
+
+    let _ver = c.Value<string> "EngineAssociation"
+    
+    let ver: EngineVersion =
+      match Version.Parse _ver with
+        | Version v -> v
+        | _ -> Guid.Parse (_ver.Trim ['{', '}'])
+
+    let va = { supported_platforms = Platform.Win64 }
+
+    va
---
+++ LibUEasyCode/Test.fs
@@ -0,0 +1,13 @@
+﻿module LibUEasyCode.Test
+
+open System
+open System.IO
+
+
+let path = "F:/UE/Project/PICOVR/PICOVR.uproject"
+
+let project_descriptor = File.ReadAllText path
+
+let d = UProject.analyse project_descriptor
+
+Console.WriteLine d.name
